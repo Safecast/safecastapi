a378fdc88d77822dff5917e63148db4c341c3d63
Brian Flanagan <briantf@me.com>
2011-10-21 16:51:18 +0100

    very simple initial device page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Safecast::Application.routes.draw do
 
   match "reading", :to => 'submissions#reading'
   match "device", :to => 'submissions#device'
+  match "details", :to => 'submissions#details'
 
   root :to => 'submissions#sign_in'
 end
