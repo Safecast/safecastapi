7e98e1e1c3cfc7a020a0970053bb38a37c04638b
Brian Flanagan <briantf@me.com>
2011-11-17 12:24:29 +0000

    restarting crappy manifest attempt

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Safecast::Application.routes.draw do
   match "reading", :to => 'submissions#reading'
   match "device", :to => 'submissions#device'
   match "details", :to => 'submissions#details'
+  match "manifest", :to => 'submissions#manifest'
 
   root :to => 'submissions#sign_in'
 end
