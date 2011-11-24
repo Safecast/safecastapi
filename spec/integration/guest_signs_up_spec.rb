010432fb4b18a83e4b79f2bb499eb020b925f443
Paul Campbell <paul@rslw.com>
2011-11-24 11:08:40 +0000

    one other little slight refactor

--- spec/integration/guest_signs_up_spec.rb
+++ spec/integration/guest_signs_up_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 feature 'guest signs up' do
   
-  def sign_up
+  def sign_up(email = 'paul@rslw.com',
+              name = 'Paul Campbell',
+              password = 'mynewpassword')
     visit('/')
-    fill_in('Email',    :with => 'paul@rslw.com')
-    fill_in('Name',     :with => 'Paul Campbell')
-    fill_in('Password', :with => 'mynewpassword')
+    fill_in('Email',    :with => email)
+    fill_in('Name',     :with => name)
+    fill_in('Password', :with => password)
     click_button('Sign in')
   end
   
