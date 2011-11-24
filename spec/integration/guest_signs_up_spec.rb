b5bda89f628737d896b8878968eb69b7f5fbc5ad
Paul Campbell <paul@rslw.com>
2011-11-24 11:07:30 +0000

    slight refactor

--- spec/integration/guest_signs_up_spec.rb
+++ spec/integration/guest_signs_up_spec.rb
@@ -2,12 +2,16 @@ require 'spec_helper'
 
 feature 'guest signs up' do
   
-  scenario 'signing up' do
+  def sign_up
     visit('/')
     fill_in('Email',    :with => 'paul@rslw.com')
     fill_in('Name',     :with => 'Paul Campbell')
     fill_in('Password', :with => 'mynewpassword')
     click_button('Sign in')
+  end
+  
+  scenario 'signing up' do
+    sign_up
     last_response.should have_content('My Dashboard')
   end
   
