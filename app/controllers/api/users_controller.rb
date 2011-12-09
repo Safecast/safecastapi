07451d772f978666a951d6b9b18700e7991f73fb
rknLA <kevin@rkn.la>
2011-12-09 12:07:06 -0800

    Implement POST /api/users enpoint
    Users can be created from the API now.
    Success returns a message, the user's email (login), and auth_key

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,4 +6,15 @@ class Api::UsersController < Api::ApplicationController
     respond_with user
   end
   
+  def create
+    u = User.create(:email => params[:email], :name => params[:name], :password => params[:password])
+    begin
+      u.save!
+      output = {:message => "User created successfully", :login => u[:email], :auth_key => u[:authentication_token]}
+      render :json => output
+    rescue
+      render :json => {:errors => u.errors.messages}
+    end
+  end
+  
 end
