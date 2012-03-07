94b2aca643aabd7138a704234616f202bbfe9a76
Paul Campbell <paul@rslw.com>
2012-03-07 23:37:32 +0000

    use fog in production

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -7,7 +7,11 @@ class FileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
