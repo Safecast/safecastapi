dbf8c5d63477eeb47fa07a70360c6006c6908612
Paul Campbell <paul@rslw.com>
2012-03-23 18:03:52 +0000

    update locations

--- app/models/drive_import.rb
+++ app/models/drive_import.rb
@@ -1,2 +1,12 @@
 class DriveImport < MeasurementImport
+  has_many :drive_logs
+  def self.update_locations
+    transaction do
+      self.find_each do |drive_import|
+        drive_import.drive_logs.find_each do |drive_log|
+          drive_log.update_location
+        end
+      end
+    end
+  end
 end
--- app/models/drive_log.rb
+++ app/models/drive_log.rb
@@ -1,2 +1,10 @@
 class DriveLog < ActiveRecord::Base
+
+  def update_location
+    self.location ||= Point.new
+    self.location.x = latitude
+    self.location.y = longitude
+    self.save!
+  end
+
 end
