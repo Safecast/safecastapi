19e77289dad901689ff6053ce13fd3fb9ae219e8
Paul Campbell <paul@rslw.com>
2012-05-26 11:38:47 +0100

    do we care if description isn't present?

--- app/models/map.rb
+++ app/models/map.rb
@@ -6,8 +6,6 @@ class Map < ActiveRecord::Base
   
   belongs_to :user
   
-  validates :description, :presence => true
-  
   def serializable_hash(options)
     options ||= {}
     super(options.merge(:only => [
