9f5b17ec401692b6396cb5f77d66b127f0bfbaff
rknLA <kevin@rkn.la>
2012-07-26 12:33:02 -0700

    Fix broken.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Safecast::Application.initialize!
-
-config.action_mailer.delivery_method = :sendmail  
-config.action_mailer.sendmail_settings = {:arguments => "-i"}
