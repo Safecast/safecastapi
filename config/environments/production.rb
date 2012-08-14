4e36f2177bc9f77a88b660006b04e3945ea020b0
Paul Campbell <paul@rslw.com>
2012-08-14 23:03:47 +0100

    Use aws/ses for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,5 @@ Safecast::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :sendmail  
-  config.action_mailer.sendmail_settings = {:arguments => "-i"}
-
-
+  config.action_mailer.delivery_method = :ses
 end
