71fd4cedbf5da3e1434d0c857f7cfc1f33a1e994
rknLA <kevin@rkn.la>
2012-09-25 14:48:23 -0700

    Fix device model spec.

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
 describe Device do
+  let(:sensor) { Fabricate(:sensor, {
+    :manufacturer => 'LND',
+    :model => '712',
+    :measurement_type => 'alpha beta gamma',
+    :measurement_category => 'radiation'
+  })}
+
   let(:device) { Fabricate(:device, {
     :manufacturer     => 'Safecast',
     :model            => 'iGeigie',
-    :sensor           => 'LND-712'
+    :sensors          => [sensor]
   })}
 
   subject { device }
 
   its(:manufacturer)    { should == 'Safecast' }
   its(:model)           { should == 'iGeigie' }
-  its(:sensor)          { should == 'LND-712' }
+  its(:sensors)          { should include(sensor) }
 end
