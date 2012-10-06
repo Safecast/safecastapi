c3306ba25c1855849304182d5cb255c071a58522
rknLA <kevin@rkn.la>
2012-10-06 16:22:09 -0700

    Add import name to import list.

--- spec/acceptance/user_views_bgeige_imports.rb
+++ spec/acceptance/user_views_bgeige_imports.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 feature "User views bGeigie imports" do
   let!(:user) { Fabricate(:user) }
-  let!(:bgeigie_import) { Fabricate(:bgeigie_import, :user => user, :status => "done") }
+  let!(:bgeigie_import) { Fabricate(:bgeigie_import, :user => user, :name => 'my import', :status => "done") }
   let!(:public_bgeigie_import) { Fabricate(:bgeigie_import, :status => "done") }
   let!(:pending_bgeigie_import) { Fabricate(:bgeigie_import) }
 
@@ -12,5 +12,10 @@ feature "User views bGeigie imports" do
     click_link("Imports")
     page.should have_content("Yours")
     page.should have_content("Public")
+
+    page.should have_content(bgeigie_import.name)
+    page.should have_content('[unnamed]') #public_bgeigie_import has no name.
   end
+
+
  end 
