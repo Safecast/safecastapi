69867d1a2353c4aa82b5fc2d878a7cd7db8c3b78
Paul Campbell <paul@rslw.com>
2013-01-14 13:36:45 +0900

    fix up profile

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,20 @@
+class ProfilesController < ApplicationController
+  
+  def show
+    @user = current_user
+  end
+  
+  def edit
+    @user = current_user
+  end
+  
+  def update
+    @user = current_user
+    if @user.update_attributes(params[:user])
+      redirect_to :profile
+    else
+      render :edit
+    end
+  end
+  
+end
