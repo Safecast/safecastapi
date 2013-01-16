768ae9094a376925a37d66e9559a392e57563f0b
Paul Campbell <paul@rslw.com>
2013-01-16 16:20:55 +0900

    add html link

---
+++ app/helpers/docs_helper.rb
@@ -0,0 +1,14 @@
+module DocsHelper
+
+  def api_example(url_method, *args)
+    json_args = args.collect { |h| (h.is_a?(Hash) ? h.merge(:format => :json) : h)}
+    json_args = [{:format => :json}] if json_args.empty?
+    json_url = send(url_method, *json_args)
+    url = send(url_method, *args)
+    render :partial => 'layouts/api_example', :locals => {
+      :json_url => json_url,
+      :url => url
+    }
+  end
+
+end
