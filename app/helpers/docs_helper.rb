58af364781e4d2c56fe08c9edf396e24e9c4367f
Paul Campbell <paul@rslw.com>
2013-01-16 17:50:44 +0900

    remove locale and add API example to devices

--- app/helpers/docs_helper.rb
+++ app/helpers/docs_helper.rb
@@ -1,7 +1,7 @@
 module DocsHelper
 
   def api_example(url_method, *args)
-    json_args = args.collect { |h| (h.is_a?(Hash) ? h.merge(:format => :json) : h)}
+    json_args = args.collect { |h| (h.is_a?(Hash) ? h.merge(:format => :json, :locale => nil) : h)}
     json_args = [{:format => :json}] if json_args.empty?
     json_url = send(url_method, *json_args)
     url = send(url_method, *args)
