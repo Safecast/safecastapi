a05078314b47c351795f42ffefdbddd440feea8c
Paul Campbell <paul@rslw.com>
2013-01-17 19:25:23 +0900

    add a filestorage independent filename method

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -20,6 +20,10 @@ class FileUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def filename
+    File.basename(self.to_s)
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
