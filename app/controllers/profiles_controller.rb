e95a4deca644719feea6f84d57ebba00a0cb4eb4
Paul Campbell <paul@rslw.com>
2013-01-17 21:14:34 +0900

    remove profile actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,5 @@
 class ProfilesController < ApplicationController
   
-  def show
-    @user = current_user
-  end
-  
-  def edit
-    @user = current_user
-  end
-  
   def update
     @user = current_user
     if @user.update_attributes(params[:user])
