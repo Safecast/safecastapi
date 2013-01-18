5b0becd47705ca7f36c860cf7b0c4cc4afc5d989
Paul Campbell <paul@rslw.com>
2013-01-18 15:25:16 +0900

    add change password

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
   def update
     @user = current_user
     if @user.update_attributes(params[:user])
-      redirect_to :profile
+      redirect_to :dashboard
     else
       render :edit
     end
