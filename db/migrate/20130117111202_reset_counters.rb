f67f75a746a4a268094acc2d9e5d17e0c51c2bd8
Paul Campbell <paul@rslw.com>
2013-01-18 16:04:50 +0900

    reset counters migration

--- db/migrate/20130117111202_reset_counters.rb
+++ db/migrate/20130117111202_reset_counters.rb
@@ -1,7 +1,7 @@
 class ResetCounters < ActiveRecord::Migration
   def up
     User.find_each { |u| User.reset_counters(u, :measurements) }
-    Device.find_each { |u| Device.reset_counters(u, :measurements) }
+    Device.find_each { |d| Device.reset_counters(d, :measurements) }
   end
 
   def down
