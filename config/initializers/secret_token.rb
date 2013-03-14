b22f1162199cd684d7914fd29451c2712038cd3e
Joe Nelson <cred+github@begriffs.com>
2013-03-14 16:49:57 -0700

    Moved secret session token into environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Safecast::Application.config.secret_token = '39c00ee18c6a2edd472f8f8b5954b6a332636e32b4c80ed9abc9053cc33bf7f1913d4cb6267ea741d10b48acfd9b3b3df2336fd02de83a4290509bc5d97c0edd'
+Safecast::Application.config.secret_token = ENV['SESSION_TOKEN']
