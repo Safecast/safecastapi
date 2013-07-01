2e46d279670873c036a403a4df2d1d59411e1cfc
Kalin KOZHUHAROV <me.kalin@gmail.com>
2013-07-01 15:39:30 +0900

    Adding measurement_import_id to the CSV dump. Some comment cleanup. This needs to be run from cron, weekly is fine for now.

--- script/dump_simple.sh
+++ script/dump_simple.sh
@@ -2,13 +2,18 @@
 
 OUTPUT_DIR=/data/teamdata/shared/system
 
-#start clean
+# FIXME: check we are not running alread, implement PID file lock
+
+# start on clean
 rm -f /tmp/simple_dataset.csv
-#dump the data
-psql -U deploy -h ec2-50-112-49-143.us-west-2.compute.amazonaws.com teamdata -c "\copy (SELECT captured_at, ST_Y(location::geometry), ST_X(location::geometry), height, value FROM measurements WHERE unit='cpm' ORDER BY captured_at) to '/tmp/simple_dataset.csv' csv;"
-#add header and bzip2 the result
-echo "timestamp,lat,lon,alt,CPM" | cat - /tmp/simple_dataset.csv |bzip2 - > ${OUTPUT_DIR}/simple_dataset.csv.bz2
-#end clean
+
+# dump the data
+psql -U deploy -h ec2-50-112-49-143.us-west-2.compute.amazonaws.com teamdata -c "\copy (SELECT captured_at, ST_Y(location::geometry), ST_X(location::geometry), height, value, measurement_import_id FROM measurements WHERE unit='cpm' ORDER BY captured_at) to '/tmp/simple_dataset.csv' csv;"
+
+# add header and bzip2 the result
+echo "timestamp,lat,lon,alt,CPM,import_id" | cat - /tmp/simple_dataset.csv |bzip2 - > ${OUTPUT_DIR}/simple_dataset.csv.bz2
+
+# clean the tmp file
 rm -f /tmp/simple_dataset.csv
 
 #
