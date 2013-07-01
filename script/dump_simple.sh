90e6561827f9b93b9e9defaf24f8fcea8857f153
Kalin KOZHUHAROV <me.kalin@gmail.com>
2013-07-01 20:18:24 +0900

    height is not altitude!

--- script/dump_simple.sh
+++ script/dump_simple.sh
@@ -2,16 +2,17 @@
 
 OUTPUT_DIR=/data/teamdata/shared/system
 
-# FIXME: check we are not running alread, implement PID file lock
+# FIXME: check we are not running already by implementing PID file lock
 
 # start on clean
 rm -f /tmp/simple_dataset.csv
 
 # dump the data
-psql -U deploy -h ec2-50-112-49-143.us-west-2.compute.amazonaws.com teamdata -c "\copy (SELECT captured_at, ST_Y(location::geometry), ST_X(location::geometry), height, value, measurement_import_id FROM measurements WHERE unit='cpm' ORDER BY captured_at) to '/tmp/simple_dataset.csv' csv;"
+# FIXME: altitude is also needed!
+psql -U deploy -h ec2-50-112-49-143.us-west-2.compute.amazonaws.com teamdata -c "\copy (SELECT captured_at, ST_Y(location::geometry), ST_X(location::geometry), value, measurement_import_id FROM measurements WHERE unit='cpm' ORDER BY captured_at) TO '/tmp/simple_dataset.csv' CSV;"
 
 # add header and bzip2 the result
-echo "timestamp,lat,lon,alt,CPM,import_id" | cat - /tmp/simple_dataset.csv |bzip2 - > ${OUTPUT_DIR}/simple_dataset.csv.bz2
+echo "timestamp,lat,lon,CPM,import_id" | cat - /tmp/simple_dataset.csv |bzip2 - > ${OUTPUT_DIR}/simple_dataset.csv.bz2
 
 # clean the tmp file
 rm -f /tmp/simple_dataset.csv
