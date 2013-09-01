19fbd6981a485a3bf7c0d5c015633952b6fdb0be
Frangible <ndolezal@gmail.com>
2013-09-01 17:52:13 -0600

    Hack for JP Post spatial inaccuracies

--- script/ios_query.sql
+++ script/ios_query.sql
@@ -4,6 +4,7 @@ BEGIN TRANSACTION;
 CREATE TEMPORARY TABLE IF NOT EXISTS Temp1(X1 INT, Y1 INT, captured_at INT2, DRE FLOAT4);
 TRUNCATE TABLE Temp1;
 
+-- first insert is for NON Japan Post (ie, normal) data
 INSERT INTO Temp1(X1, Y1, captured_at, DRE)
 SELECT CAST((CAST(ST_X(location::geometry) AS FLOAT)+180.0)/360.0*2097152.0+0.5 AS INT) AS X1
     ,CAST((0.5-LN((1.0+SIN(CAST(ST_Y(location::geometry) AS FLOAT)*pi()/180.0))/(1.0-SIN(CAST(ST_Y(location::geometry) AS FLOAT)*pi()/180.0)))/(4.0*pi()))*2097152.0+0.5 AS INT) AS Y1
@@ -18,7 +19,8 @@ SELECT CAST((CAST(ST_X(location::geometry) AS FLOAT)+180.0)/360.0*2097152.0+0.5 
         ELSE 0.0
     END AS DRE
 FROM measurements
-WHERE captured_at IS NOT NULL
+WHERE user_id != 347
+    AND captured_at IS NOT NULL
     AND captured_at > TIMESTAMP '2011-03-01 00:00:00'
     AND captured_at < localtimestamp + interval '48 hours'
     AND ST_X(location::geometry) IS NOT NULL
@@ -28,6 +30,41 @@ WHERE captured_at IS NOT NULL
     AND (  CAST(ST_X(location::geometry) AS FLOAT) != 0.0
         OR CAST(ST_Y(location::geometry) AS FLOAT) != 0.0)
     AND (  (unit='cpm' AND value > 10.0 AND value < 30000.0)
+        OR (unit='microsievert' AND value > 0.02 AND value < 75.0))
+    AND CAST(ST_Y(location::geometry) AS FLOAT) < 85.05
+    AND CAST(ST_Y(location::geometry) AS FLOAT) > -85.05
+    AND CAST(ST_X(location::geometry) AS FLOAT) >= -180.0
+    AND CAST(ST_X(location::geometry) AS FLOAT) <=  180.0;
+    
+-- This 2nd insert is a hack workaround for JP post data
+-- -10k to the "days since 1970" is approx a -30 year penalty to the date restrictive binning so as not to
+--       contaminate measurements with superior spatial resolution
+-- Although that's a little unfair to 1970, because LANDSAT 5 had better resolution than the Japan Post.
+INSERT INTO Temp1(X1, Y1, captured_at, DRE)
+SELECT CAST((CAST(ST_X(location::geometry) AS FLOAT)+180.0)/360.0*2097152.0+0.5 AS INT)+2 AS X1
+    ,CAST((0.5-LN((1.0+SIN(CAST(ST_Y(location::geometry) AS FLOAT)*pi()/180.0))/(1.0-SIN(CAST(ST_Y(location::geometry) AS FLOAT)*pi()/180.0)))/(4.0*pi()))*2097152.0+0.5 AS INT)-2 AS Y1
+    ,CAST(EXTRACT(epoch FROM captured_at)/86400 AS INT2)-10950 AS captured_at
+    ,CASE
+        WHEN unit='cpm' AND device_id IS NULL THEN value/350.0
+        WHEN unit='microsievert' THEN value
+        WHEN unit='cpm' AND device_id IN (5,15,16,17,18,22) THEN value/350.0
+        WHEN unit='cpm' AND device_id IN (6,7,11,13,23) THEN value/100.0
+        WHEN unit='cpm' AND device_id IN (4,9,10,12,19,24) THEN value/132.0
+        WHEN unit='cpm' AND device_id IN (21) THEN value/1750.0
+        ELSE 0.0
+    END AS DRE
+FROM measurements
+WHERE user_id = 347
+    AND captured_at IS NOT NULL
+    AND captured_at > TIMESTAMP '2011-03-01 00:00:00'
+    AND captured_at < localtimestamp + interval '48 hours'
+    AND ST_X(location::geometry) IS NOT NULL
+    AND ST_Y(location::geometry) IS NOT NULL
+    AND value IS NOT NULL
+    AND (device_id IS NULL OR device_id <= 24 OR unit='microsievert')
+    AND (  CAST(ST_X(location::geometry) AS FLOAT) != 0.0
+        OR CAST(ST_Y(location::geometry) AS FLOAT) != 0.0)
+    AND (  (unit='cpm' AND value > 14.0 AND value < 30000.0)
         OR (unit='microsievert' AND value > 0.02 AND value < 75.0))
     AND CAST(ST_Y(location::geometry) AS FLOAT) < 85.05
     AND CAST(ST_Y(location::geometry) AS FLOAT) > -85.05
