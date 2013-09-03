17d62fae92eee79931f02f9e504e346178c6e03e
Frangible <ndolezal@gmail.com>
2013-09-02 22:25:10 -0600

    microsievert =~ usv
    Though both are wrong, that's dose not dose rate... oh well.

--- script/ios_query.sql
+++ script/ios_query.sql
@@ -12,7 +12,7 @@ SELECT CAST((CAST(ST_X(location::geometry) AS FLOAT)+180.0)/360.0*2097152.0+0.5 
     ,CAST(EXTRACT(epoch FROM captured_at)/86400 AS INT2) AS captured_at
     ,CASE
         WHEN unit='cpm' AND device_id IS NULL THEN value/350.0
-        WHEN unit='microsievert' THEN value
+        WHEN unit IN ('microsievert','usv') THEN value
         WHEN unit='cpm' AND device_id IN (5,15,16,17,18,22) THEN value/350.0
         WHEN unit='cpm' AND device_id IN (6,7,11,13,23) THEN value/100.0
         WHEN unit='cpm' AND device_id IN (4,9,10,12,19,24) THEN value/132.0
@@ -27,11 +27,11 @@ WHERE user_id NOT IN (345,347)
     AND ST_X(location::geometry) IS NOT NULL
     AND ST_Y(location::geometry) IS NOT NULL
     AND value IS NOT NULL
-    AND (device_id IS NULL OR device_id <= 24 OR unit='microsievert')
+    AND (device_id IS NULL OR device_id <= 24 OR unit IN ('microsievert','usv'))
     AND (  CAST(ST_X(location::geometry) AS FLOAT) != 0.0
         OR CAST(ST_Y(location::geometry) AS FLOAT) != 0.0)
     AND (  (unit='cpm' AND value > 10.0 AND value < 30000.0)
-        OR (unit='microsievert' AND value > 0.02 AND value < 75.0))
+        OR (unit IN ('microsievert','usv') AND value > 0.02 AND value < 75.0))
     AND id NOT IN (13194822,15768545,15768817,15690104,15768346,15768782,15768792,16381794,18001818,17342620,14669786,25389168,25389158,25389157,25389153,24482487,16537265,16537266,19554057,19554058,19554059,19554060,19555677,19589301,19589302,19589303,19589304,19589305,19589303,19589304,19589305,19600634,19699406,17461603,17461607,17461611,17461615,16981355)
     AND (id < 23181608 OR id > 23182462)
     AND CAST(ST_Y(location::geometry) AS FLOAT) < 85.05
@@ -49,7 +49,7 @@ SELECT CAST((CAST(ST_X(location::geometry) AS FLOAT)+180.0)/360.0*2097152.0+0.5 
     ,CAST(EXTRACT(epoch FROM captured_at)/86400 AS INT2)-10950 AS captured_at
     ,CASE
         WHEN unit='cpm' AND device_id IS NULL THEN value/350.0
-        WHEN unit='microsievert' THEN value
+        WHEN unit IN ('microsievert','usv') THEN value
         WHEN unit='cpm' AND device_id IN (5,15,16,17,18,22) THEN value/350.0
         WHEN unit='cpm' AND device_id IN (6,7,11,13,23) THEN value/100.0
         WHEN unit='cpm' AND device_id IN (4,9,10,12,19,24) THEN value/132.0
@@ -64,11 +64,11 @@ WHERE user_id = 347
     AND ST_X(location::geometry) IS NOT NULL
     AND ST_Y(location::geometry) IS NOT NULL
     AND value IS NOT NULL
-    AND (device_id IS NULL OR device_id <= 24 OR unit='microsievert')
+    AND (device_id IS NULL OR device_id <= 24 OR unit IN ('microsievert','usv'))
     AND (  CAST(ST_X(location::geometry) AS FLOAT) != 0.0
         OR CAST(ST_Y(location::geometry) AS FLOAT) != 0.0)
     AND (  (unit='cpm' AND value > 17.0 AND value < 30000.0)
-        OR (unit='microsievert' AND value > 0.02 AND value < 75.0))
+        OR (unit IN ('microsievert','usv') AND value > 0.02 AND value < 75.0))
     AND CAST(ST_Y(location::geometry) AS FLOAT) < 85.05
     AND CAST(ST_Y(location::geometry) AS FLOAT) > -85.05
     AND CAST(ST_X(location::geometry) AS FLOAT) >= -180.0
