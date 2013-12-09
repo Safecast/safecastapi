22bdbdc49cd0e5fc4951fa3167bbd4741d63078a
Frangible <ndolezal@gmail.com>
2013-12-09 11:07:43 -0700

    Test #2
    Previous attempt resulted in failure due to reasons unknown, retrying with modifications and without original CSV export.  This test will attempt to determine if the time interval or multiple commands have any directly negative effect upon the output result.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,7 @@
 env :PATH, ENV['PATH']
 
-every 240.minutes, :roles => [:app] do
+every 60.minutes, :roles => [:app] do
   command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements_ios"
-  command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements"
 end
 
 # every 45.minutes, :roles => [:app] do
