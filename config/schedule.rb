b2091c1985fde92c4bfe1aa88cabe63ba6139752
Frangible <ndolezal@gmail.com>
2013-12-09 00:20:17 -0700

    Break production code for SCIENCE!
    Trial and error: usually ends in error.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,14 @@
 env :PATH, ENV['PATH']
 
-every 45.minutes, :roles => [:app] do
+every 240.minutes, :roles => [:app] do
+  command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements_ios"
   command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements"
 end
 
-every 4.hours, :roles => [:app] do
-  command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements_ios"
-end
+# every 45.minutes, :roles => [:app] do
+#   command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements"
+# end
+
+# every 4.hours, :roles => [:app] do
+#  command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements_ios"
+# end
