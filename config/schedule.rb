ac0183e024b83ce12ee6634efacc6d57f2e1f106
Frangible <ndolezal@gmail.com>
2013-12-09 19:00:56 -0700

    All attempts failed.  Reverting.
    sadface.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,9 @@
 env :PATH, ENV['PATH']
 
-every 60.minutes, :roles => [:app] do
-  command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements_ios"
+every 45.minutes, :roles => [:app] do
+  command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements"
 end
 
-# every 45.minutes, :roles => [:app] do
-#   command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements"
-# end
-
-# every 4.hours, :roles => [:app] do
-#  command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements_ios"
-# end
+every 4.hours, :roles => [:app] do
+  command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements_ios"
+end
