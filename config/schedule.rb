eb5ce2c691dc88df11b358863d915e7b4decf5bd
Frangible <ndolezal@gmail.com>
2013-12-09 19:48:47 -0700

    Moving iOS update command to C66 cron job
    It worked with cron, nuff said?

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,6 @@ every 45.minutes, :roles => [:app] do
   command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements"
 end
 
-every 4.hours, :roles => [:app] do
-  command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements_ios"
-end
+# every 4.hours, :roles => [:app] do
+#  command "/var/deploy/api.safecast.org/web_head/current/script/dump_measurements_ios"
+# end
