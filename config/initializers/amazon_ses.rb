7d7bf8e26dad2d365aff5756aa0c1c192900878a
Rob Oudendijk <rob@yr-design.biz>
2015-06-02 02:25:08 +0900

    Update amazon_ses.rb

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -2,4 +2,3 @@ ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   :access_key_id     => Configurable.aws_access_key_id,
   :secret_access_key => Configurable.aws_secret_access_key
   
-# ses = AWS::SES::Base.new(:access_key_id     => 'AKIAIUP5TO7IEWCLDR7Q', :secret_access_key => '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')
