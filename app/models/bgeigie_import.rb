12a2aef961e73fbd95988bed38b612daf32aeceb
Mat Schaffer <mat@schaffer.me>
2015-06-04 15:42:15 +0900

    Quiet down test output

--- app/models/bgeigie_import.rb
+++ app/models/bgeigie_import.rb
@@ -10,10 +10,10 @@ class BgeigieImport < MeasurementImport
   validates :user, :presence => true, :on => :create
   validates :cities, :presence => true, :on => :update
   validates :credits, :presence => true, :on => :update
-  
+
   belongs_to :user
   has_many :bgeigie_logs, :dependent => :delete_all
-  
+
   scope :newest, order("created_at DESC")
   scope :oldest, order("created_at")
   scope :done, where(:status => "done")
@@ -29,7 +29,7 @@ class BgeigieImport < MeasurementImport
   def self.filter(query)
     where("lower(name) LIKE :query
            OR lower(source) LIKE :query
-           OR lower(description) LIKE :query 
+           OR lower(description) LIKE :query
            OR lower(cities) LIKE :query
            OR lower(credits) LIKE :query", :query => "%#{query.downcase}%")
   end
@@ -57,12 +57,12 @@ class BgeigieImport < MeasurementImport
   def tmp_file
     @tmp_file ||= "/tmp/bgeigie-#{Kernel.rand}"
   end
-  
+
   def confirm_status(item)
     self.status_details[item] = true
     self.save!(:validate => false)
   end
-  
+
   def process
     create_tmp_file
     import_to_bgeigie_logs
@@ -80,7 +80,7 @@ class BgeigieImport < MeasurementImport
     Delayed::Job.enqueue FinalizeBgeigieImportJob.new(id)
     Notifications.import_approved(self).deliver
   end
-  
+
   def finalize!
     import_measurements
     update_counter_caches
@@ -110,9 +110,9 @@ class BgeigieImport < MeasurementImport
     #check header
     return false unless line_items[0].eql? '$BMRDD' or line_items[0].eql? '$BGRDD' or line_items[0].eql? '$BNRDD' or line_items[0].eql? '$BNXRDD'
 
-    #check for Valid CPM 
+    #check for Valid CPM
     return false unless line_items[6].eql? 'A' or line_items[6].eql? 'V'
-    
+
     #check for GPS lock
     return false unless line_items[12].eql? 'A' or line_items[12].eql? 'V'
 
@@ -141,8 +141,8 @@ class BgeigieImport < MeasurementImport
     %Q[psql -U #{db_config['username']} -h #{db_config['host'] || 'localhost'} #{db_config['database']} -c "\\copy bgeigie_logs_tmp (device_tag, device_serial_id, captured_at, cpm, counts_per_five_seconds, total_counts,  cpm_validity, latitude_nmea, north_south_indicator, longitude_nmea,  east_west_indicator, altitude, gps_fix_indicator,horizontal_dilution_of_precision,  gps_fix_quality_indicator,md5sum) FROM '#{tmp_file}' CSV"]
   end
 
-  def run_psql_command    
-    system(psql_command)
+  def run_psql_command
+    logger.info `#{psql_command}`
   end
 
   def drop_and_create_tmp_table
@@ -161,7 +161,7 @@ class BgeigieImport < MeasurementImport
   def drop_tmp_table
     self.connection.execute("DROP TABLE bgeigie_logs_tmp")
   end
-  
+
   def import_to_bgeigie_logs
     drop_and_create_tmp_table
     run_psql_command
@@ -183,14 +183,14 @@ class BgeigieImport < MeasurementImport
             'POINT (',
             (
               (floor(abs(longitude_nmea) / 100)
-              + 
+              +
               (mod(abs(longitude_nmea),100)/60))
               *
               (case when east_west_indicator = 'W' then -1 else  1 end)
             ), ' ',
             (
               (floor(abs(latitude_nmea) / 100)
-              + 
+              +
               (mod(abs(latitude_nmea),100)/60))
               *
               (case when north_south_indicator = 'S' then -1 else  1 end)
@@ -199,7 +199,7 @@ class BgeigieImport < MeasurementImport
         );
     ])
   end
-  
+
   def import_measurements
     self.connection.execute(%Q[
       insert into measurements
@@ -215,14 +215,14 @@ class BgeigieImport < MeasurementImport
     return unless user.present?
     User.reset_counters(user, :measurements)
   end
-  
+
   def delete_tmp_file
     File.unlink(tmp_file)
   end
 
   def nmea_to_lat_lng(latitude_nmea, north_south_indicator, longitude_nmea, east_west_indicator)
     #algorithm described at http://notinthemanual.blogspot.com/2008/07/convert-nmea-latitude-longitude-to.html
-    
+
     #protect against buggy nmea values that have negative values
     latitude_nmea = b.latitude_nmea.abs
     longitude_nmea = b.longitude_nmea.abs
@@ -239,10 +239,10 @@ class BgeigieImport < MeasurementImport
 
     lat_degrees = (latitude_nmea / 100).to_i
     lng_degrees = (longitude_nmea / 100).to_i
-    
+
     lat_decimal = (latitude_nmea % 100) / 60
     lng_decimal = (longitude_nmea % 100) / 60
-    
+
     {
       :latitude => (lat_degrees + lat_decimal) * lat_sign,
       :longitude => (lng_degrees + lng_decimal) * lng_sign
