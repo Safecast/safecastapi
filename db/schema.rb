d2abc08022f2b417d6ce205ab6abd32e6c9ccd31
Rob Oudendijk <rob@yr-design.biz>
2015-09-17 20:05:12 +0900

    Update schema.rb
    added extra fields.. migration files still needs to be created

--- db/schema.rb
+++ db/schema.rb
@@ -181,6 +181,10 @@ ActiveRecord::Schema.define(:version => 20140718095222) do
     t.integer  "height"
     t.string   "surface"
     t.string   "radiation"
+    t.string   "devicetype_id"
+    t.integer  "sensor_id"
+    t.integer  "channel_id"
+    t.integer  "station_id"
   end
 
   add_index "measurements", ["captured_at"], :name => "index_measurements_on_captured_at"
