22338581382f19fc4a7479a3637637536a217d83
Mat Schaffer <mat@schaffer.me>
2015-10-08 15:39:43 +0900

    Some login seeds for local development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+moderator = User.new(name: 'Safecast Moderator', email: 'safecast.moderator.1@mailinator.com', password: 'abc123', password_confirmation: 'abc123')
+moderator.confirmed_at = Time.now
+moderator.moderator = true
+moderator.save!
+
+uploader = User.new(name: 'Safecast Uploader', email: 'safecast.uploader.1@mailinator.com', password: 'abc123', password_confirmation: 'abc123')
+uploader.confirmed_at = Time.now
+uploader.save!
