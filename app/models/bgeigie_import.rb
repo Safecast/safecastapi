35145e9be4123fc912b60786728aefe810212a38
Mat Schaffer <mat@schaffer.me>
2015-10-08 14:12:26 +0900

    Update nmea_to_lat_lng to use args
    We don't use this function in the app, but it's handy at the moment for doing a first-pass nmea to floating point degrees conversion

--- app/models/bgeigie_import.rb
+++ app/models/bgeigie_import.rb
@@ -220,16 +220,16 @@ class BgeigieImport < MeasurementImport
     #algorithm described at http://notinthemanual.blogspot.com/2008/07/convert-nmea-latitude-longitude-to.html
 
     #protect against buggy nmea values that have negative values
-    latitude_nmea = b.latitude_nmea.abs
-    longitude_nmea = b.longitude_nmea.abs
+    latitude_nmea = latitude_nmea.abs
+    longitude_nmea = longitude_nmea.abs
 
     lat_sign = 1
-    if b.north_south_indicator == 'S'
+    if north_south_indicator == 'S'
       lat_sign = -1
     end
 
     lng_sign = 1
-    if b.east_west_indicator == 'W'
+    if east_west_indicator == 'W'
       lng_sign = -1
     end
 
