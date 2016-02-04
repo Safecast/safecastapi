7e6ca102c4a48e2148eeb467cf5a4868fd1a8601
Mat Schaffer <mat@schaffer.me>
2016-02-04 11:32:08 +0900

    Run skylight on staging env

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,8 @@ module Safecast
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.skylight.environments += ['staging']
+
     config.generators do |g|
       g.test_framework :rspec,
                        :fixture => false,
