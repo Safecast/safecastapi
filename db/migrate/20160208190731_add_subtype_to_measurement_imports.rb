51988478d689061234d0e55f8198d5254757c7fe
Mat Schaffer <mat@schaffer.me>
2016-04-11 22:12:16 -0700

    Add indexes and default per feedback

--- db/migrate/20160208190731_add_subtype_to_measurement_imports.rb
+++ db/migrate/20160208190731_add_subtype_to_measurement_imports.rb
@@ -1,5 +1,7 @@
 class AddSubtypeToMeasurementImports < ActiveRecord::Migration
   def change
-    add_column :measurement_imports, :subtype, :string
+    add_column :measurement_imports, :subtype, :string, default: 'None', null: false
+    add_index :measurement_imports, :subtype
+    add_index :measurement_imports, [ :id, :subtype ]
   end
 end
