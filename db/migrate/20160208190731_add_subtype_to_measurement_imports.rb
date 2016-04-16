442a243038c9a7925085f3dcd80b71191d4e7628
Mat Schaffer <mat@schaffer.me>
2016-04-15 19:48:27 -0700

    Switch subtype to enum

--- db/migrate/20160208190731_add_subtype_to_measurement_imports.rb
+++ db/migrate/20160208190731_add_subtype_to_measurement_imports.rb
@@ -1,7 +1,21 @@
 class AddSubtypeToMeasurementImports < ActiveRecord::Migration
-  def change
-    add_column :measurement_imports, :subtype, :string, default: 'None', null: false
+  def up
+    execute <<-SQL
+      CREATE TYPE measurement_imports_subtype AS ENUM ('None', 'Drive', 'Surface', 'Cosmic');
+      ALTER TABLE measurement_imports
+        ADD COLUMN subtype measurement_imports_subtype
+        NOT NULL
+        DEFAULT 'None';
+    SQL
     add_index :measurement_imports, :subtype
     add_index :measurement_imports, [ :id, :subtype ]
+  end
+
+  def down
+    execute <<-SQL
+      ALTER TABLE measurement_imports
+        DROP COLUMN subtype;
+      DROP TYPE measurement_imports_subtype;
+    SQL
   end
 end
