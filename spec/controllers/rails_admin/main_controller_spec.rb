08191bc606117af08304f7c4dc9fa4e69ab165f7
Eito Katagiri <eitoball@gmail.com>
2016-06-03 10:47:01 +0900

    add spec that normal user is redirected root path when accessing admin interface

--- spec/controllers/rails_admin/main_controller_spec.rb
+++ spec/controllers/rails_admin/main_controller_spec.rb
@@ -59,3 +59,22 @@ RSpec.describe RailsAdmin::MainController, 'login as admin', type: :controller d
     it { expect(response).to be_ok }
   end
 end
+
+RSpec.describe RailsAdmin::MainController, 'login as user', type: :controller do
+  before do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+
+    sign_in Fabricate(:user)
+  end
+
+  let!(:bgeigie_log) { BgeigieLog.create }
+  let(:model_name) { 'bgeigie_log' }
+
+  describe 'GET #dashboard' do
+    before do
+      get :dashboard
+    end
+
+    it { expect(response).to redirect_to('/en-US') }
+  end
+end
