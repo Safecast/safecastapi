5aeadc256f990991539abe3849cd8aa183960d38
Eito Katagiri <eitoball@gmail.com>
2016-06-07 08:48:33 +0900

    :tractor: tidy up little bit

--- spec/models/bgeigie_import_spec.rb
+++ spec/models/bgeigie_import_spec.rb
@@ -1,16 +1,13 @@
-require 'spec_helper'
-
 RSpec.describe BgeigieImport, type: :model do
-  
-  let!(:user) { User.first || Fabricate(:user) }
-  
+  let(:user) { Fabricate(:user) }
+
   let!(:bgeigie_import) do
     Fabricate(:bgeigie_import,
               :source => File.new(Rails.root.join('spec/fixtures/bgeigie.log')),
               :user_id => user.id
              )
   end
-  
+
   describe "#process" do
     before(:each) do
       bgeigie_import.process
@@ -24,20 +21,20 @@ RSpec.describe BgeigieImport, type: :model do
     it "should count the number of lines in the file" do
       expect(bgeigie_import.lines_count).to eq(23)
     end
-    
+
     it "should set the id" do
       expect(BgeigieLog.all.collect { |bl| bl.bgeigie_import_id }.uniq).to eq([bgeigie_import.id])
     end
-    
+
     it "should create measurements" do
       expect(Measurement.count).to eq(23)
     end
-    
+
     it "should not load them twice" do
       bgeigie_import.process
       expect(Measurement.count).to eq(23)
     end
-    
+
     it "should set measurement attributes" do
       measurement = Measurement.find_by_md5sum('6750a7cf2a630c2dde416dc7d138fa74')
       expect(measurement.location).not_to be_blank
@@ -57,7 +54,6 @@ RSpec.describe BgeigieImport, type: :model do
       expect(measurement.location.x).to eq(-73.92277166666666)
       expect(measurement.location.y).to eq(41.698078333333335)
     end
-
 
     it "should calculate measurements to the correct hemisphere" do
       bgeigie_with_bugs = Fabricate(:bgeigie_import,
@@ -82,10 +78,6 @@ RSpec.describe BgeigieImport, type: :model do
       bgeigie_with_bugs.finalize!
 
       expect(Measurement.all.count).to eq(30) #the before :each import has 23, and the corrupted file should have 7 valid measurements
-
     end
-    
   end
-  
-  after(:each) { BgeigieLog.destroy_all }
 end
