73da1745730140d47e9d573c1197cc51944bc619
Eito Katagiri <eitoball@gmail.com>
2016-06-07 14:24:11 +0900

    change for consistency

--- app/models/bgeigie_import.rb
+++ app/models/bgeigie_import.rb
@@ -50,10 +50,8 @@ class BgeigieImport < MeasurementImport
     where("created_at > ?", time)
   end
 
-  class << self
-    def by_subtype(type_or_types)
-      where(subtype: type_or_types)
-    end
+  def self.by_subtype(type_or_types)
+    where(subtype: type_or_types)
   end
 
   def name
