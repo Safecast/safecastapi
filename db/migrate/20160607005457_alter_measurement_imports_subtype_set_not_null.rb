578f6c1a8e587985fcb905e0c67790a1f61f8f81
Mat Schaffer <mat@schaffer.me>
2016-06-07 15:16:51 +0900

    Include migration step to ensure we have no null subtypes

--- db/migrate/20160607005457_alter_measurement_imports_subtype_set_not_null.rb
+++ db/migrate/20160607005457_alter_measurement_imports_subtype_set_not_null.rb
@@ -1,6 +1,7 @@
 class AlterMeasurementImportsSubtypeSetNotNull < ActiveRecord::Migration
   def up
     execute <<-SQL
+      UPDATE measurement_imports SET subtype = 'None' WHERE subtype IS NULL;
       ALTER TABLE measurement_imports ALTER COLUMN subtype SET NOT NULL;
     SQL
   end
