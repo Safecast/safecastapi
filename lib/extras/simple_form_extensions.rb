cf819e7d4d22220117befdb6d66ebd36c222dfcb
Eito Katagiri <eitoball@gmail.com>
2016-06-11 13:29:55 +0900

    :police_car: fix Lint/AssignmentInCondition rubocop offence

--- lib/extras/simple_form_extensions.rb
+++ lib/extras/simple_form_extensions.rb
@@ -6,7 +6,8 @@ module WrappedButton
       options[:"data-loading-text"] = [loading, options[:"data-loading-text"]].compact
       options[:class] = ['btn-primary', options[:class]].compact
       args << options
-      if cancel = options.delete(:cancel)
+      cancel = options.delete(:cancel)
+      if cancel
         cancel_options = options.delete(:cancel_options)
         submit(*args, &block) + ' ' + I18n.t('simple_form.buttons.or') + ' ' + template.link_to(I18n.t('simple_form.buttons.cancel'), cancel, cancel_options)
       else
