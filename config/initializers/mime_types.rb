71c0672561de4c4d6b25108c1d7ef87b5ab538a1
Eito Katagiri <eitoball@gmail.com>
2016-06-14 15:55:34 +0900

    register each mime type, not in loop
    https://github.com/Safecast/safecastapi/pull/287#discussion_r66909873

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,7 +1,3 @@
-{
-  safecast_api_v1_json: 'application/vnd.safecast+json;version=1.0',
-  safecast_api_v1_xml: 'application/vnd.safecast+xml;version=1.0',
-  kml: 'application/vnd.google-earth.kml+xml'
-}.each do |sym, str|
-  Mime::Type.register(str, sym)
-end
+Mime::Type.register 'application/vnd.safecast+json;version=1.0', :safecast_api_v1_json
+Mime::Type.register 'application/vnd.safecast+xml;version=1.0', :safecast_api_v1_xml
+Mime::Type.register 'application/vnd.google-earth.kml+xml', :kml
