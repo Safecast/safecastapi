013bc8ee7a4f32d155f2bf8ca86ef91f5d0a13dd
Mat Schaffer <mat@schaffer.me>
2016-06-14 13:52:50 +0900

    :police_car:

--- db/migrate/20160614042818_add_user_id_and_created_at_index_to_measurements.rb
+++ db/migrate/20160614042818_add_user_id_and_created_at_index_to_measurements.rb
@@ -1,5 +1,5 @@
 class AddUserIdAndCreatedAtIndexToMeasurements < ActiveRecord::Migration
   def change
-    add_index :measurements, [ :user_id, :captured_at ]
+    add_index :measurements, [:user_id, :captured_at]
   end
 end
