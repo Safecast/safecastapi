6d484b69056872519520c6f02fea86cbb16852fe
Eito Katagiri <eitoball@gmail.com>
2016-06-23 16:32:39 +0900

    fix "Missing semicolon"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,8 +16,8 @@
 //= require_tree ./application
 //- require_self
 
-jQuery.ajaxSetup({ 
-  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept",
-    "text/javascript")} 
-})  
-
+jQuery.ajaxSetup({
+  'beforeSend': function(xhr) {
+    xhr.setRequestHeader("Accept", "text/javascript");
+  }
+});
