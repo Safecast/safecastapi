0c7cc4eaeef4ec1de6ed51c45b01768cfbcc187b
Eito Katagiri <eitoball@gmail.com>
2016-06-24 01:00:12 +0900

    :police_car: enable Style/MultilineMethodCallIndentation and auto-correct

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -26,7 +26,7 @@ class Measurement < ActiveRecord::Base
   def self.nearby_to(lat, lng, distance)
     return scoped unless lat.present? && lng.present? && distance.present?
     where("ST_DWithin(location, ST_GeogFromText('POINT (#{lng.to_f} #{lat.to_f})'), ?)", distance.to_i)
-    .order("ST_Distance(location, ST_GeogFromText('POINT (#{lng.to_f} #{lat.to_f})')) ASC")
+      .order("ST_Distance(location, ST_GeogFromText('POINT (#{lng.to_f} #{lat.to_f})')) ASC")
   end
 
   def self.by_unit(unit)
