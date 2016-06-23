f6a2d90b4705f48084765848b214aa1b1d80cca1
Eito Katagiri <eitoball@gmail.com>
2016-06-24 01:13:21 +0900

    :police_car: enable Style/PredicateName and disable explicitly

--- app/models/bgeigie_import.rb
+++ app/models/bgeigie_import.rb
@@ -153,7 +153,7 @@ class BgeigieImport < MeasurementImport # rubocop:disable Metrics/ClassLength
     confirm_status(:process_file)
   end
 
-  def is_sane?(line) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
+  def is_sane?(line) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity, Style/PredicateName
     line_items = line.strip.split(',')
     return false unless line_items.length >= 13
 
