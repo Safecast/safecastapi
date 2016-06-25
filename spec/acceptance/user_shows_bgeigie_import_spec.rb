ba8ea4988b626a1feee548d95f3dc1e505a322ea
Eito Katagiri <eitoball@gmail.com>
2016-06-25 16:39:03 +0900

    show delete import link only when user has rights to delete

---
+++ spec/acceptance/user_shows_bgeigie_import_spec.rb
@@ -0,0 +1,27 @@
+feature "Showing a bGeigie import", type: :feature do
+  let(:user) { Fabricate(:user) }
+  let(:bgeigie_import) { Fabricate(:bgeigie_import, user: user) }
+
+  scenario 'login user sees link to delete own import' do
+    sign_in user
+    visit bgeigie_import_path(bgeigie_import, locale: 'en-US')
+    expect(page).to have_xpath('//a[text()="Delete this Import"]')
+  end
+
+  scenario 'moderator user sees link to delete any import' do
+    sign_in Fabricate(:admin_user)
+    visit bgeigie_import_path(bgeigie_import, locale: 'en-US')
+    expect(page).to have_xpath('//a[text()="Delete this Import"]')
+  end
+
+  scenario 'non-login user does not see link to delete any import' do
+    visit bgeigie_import_path(bgeigie_import, locale: 'en-US')
+    expect(page).to_not have_xpath('//a[text()="Delete this Import"]')
+  end
+
+  scenario 'login user does not see link to delete other import' do
+    sign_in Fabricate(:user)
+    visit bgeigie_import_path(bgeigie_import, locale: 'en-US')
+    expect(page).to_not have_xpath('//a[text()="Delete this Import"]')
+  end
+end
