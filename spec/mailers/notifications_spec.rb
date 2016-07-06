bf081d596e68e007cc471a5e08fdd617b4142124
Eito Katagiri <eitoball@gmail.com>
2016-07-06 12:12:46 +0900

    fix to not raise exception when no moderator exists

--- spec/mailers/notifications_spec.rb
+++ spec/mailers/notifications_spec.rb
@@ -1,34 +1,12 @@
 require 'spec_helper'
 
 RSpec.describe Notifications, type: :mailer do
-  # describe "import_approved" do
-  #   let(:mail) { Notifications.import_approved }
-
-  # describe "import_rejected" do
-  #   let(:mail) { Notifications.import_rejected }
-
-  #   it "renders the headers" do
-  #     mail.subject.should eq("Import approved")
-  #     mail.to.should eq(["to@example.org"])
-  #     mail.from.should eq(["from@example.com"])
-  #   end
-
-  #   it "renders the body" do
-  #     mail.body.encoded.should match("Hi")
-  #   end
-  # end
-
-  # describe "import_awaiting_approval" do
-  #   let(:mail) { Notifications.import_awaiting_approval }
-
-  #   it "renders the headers" do
-  #     mail.subject.should eq("Import awaiting approval")
-  #     mail.to.should eq(["to@example.org"])
-  #     mail.from.should eq(["from@example.com"])
-  #   end
-
-  #   it "renders the body" do
-  #     mail.body.encoded.should match("Hi")
-  #   end
-  # end
+  describe '.import_awaiting_approval' do
+    context 'no moderator' do
+      it 'should not raise error' do
+        expect { described_class.import_awaiting_approval(nil) }
+          .to_not raise_error
+      end
+    end
+  end
 end
