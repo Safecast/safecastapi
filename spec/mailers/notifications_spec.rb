dfcc5492074ab151e0afdf9909c0e8f9a52dd1e3
Eito Katagiri <eitoball@gmail.com>
2016-07-26 10:54:02 +0900

    add specs for Notification

--- spec/mailers/notifications_spec.rb
+++ spec/mailers/notifications_spec.rb
@@ -9,4 +9,30 @@ RSpec.describe Notifications, type: :mailer do
       end
     end
   end
+
+  describe '.send_email' do
+    let(:import) do
+      double('import').tap do |d|
+        expect(d).to receive(:user).and_return(double('user', email: 'user@example.org'))
+        expect(d).to receive(:filename).and_return('filename.txt')
+      end
+    end
+
+    subject { described_class.send_email(import, 'e-mail body', 'sender@example.org') }
+
+    it { is_expected.to be_present }
+  end
+
+  describe '.contact_moderator' do
+    let(:import) do
+      double('import').tap do |d|
+        expect(d).to receive(:rejected_by).and_return('admin@example.org')
+        expect(d).to receive(:filename).and_return('filename.txt')
+      end
+    end
+
+    subject { described_class.contact_moderator(import, 'e-mail body', 'sender@example.org') }
+
+    it { is_expected.to be_present }
+  end
 end
