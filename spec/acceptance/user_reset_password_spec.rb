83ce76fdc6326dc298eff76d74cfb2982b245388
Eito Katagiri <eitoball@gmail.com>
2016-09-02 12:46:51 +0900

    fix to embed correct reset password token in insturction mail (#328)

---
+++ spec/acceptance/user_reset_password_spec.rb
@@ -0,0 +1,25 @@
+feature 'User resets password', type: :feature do
+  let(:user) { Fabricate(:user, default_locale: 'en-US') }
+
+  scenario 'User creates new measurement in uSv/h and view it in index page with filter' do
+    visit(new_user_password_path)
+
+    fill_in('Email', with: user.email)
+    click_on('Send me reset password instruction')
+
+    visit(reset_password_path_from_instruction_mail)
+
+    fill_in('New password', with: 'secret1234')
+    fill_in('Confirm new password', with: 'secret1234')
+    click_on('Change my password')
+
+    expect(page).to have_text('Your password has been changed successfully.')
+  end
+
+  def reset_password_path_from_instruction_mail
+    mail = ActionMailer::Base.deliveries.last
+    doc = Nokogiri.HTML(mail.body.raw_source)
+    uri = URI(doc.xpath('//a').attribute('href').value)
+    [uri.path, uri.query].join('?')
+  end
+end
