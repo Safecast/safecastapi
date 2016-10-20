f4f7d88daab34efdd5d3763f470deda3374af55f
Mat Schaffer <mat@schaffer.me>
2016-10-20 09:58:26 +0900

    Remove nil moderator test
    Doesn't make much sense now that moderators list is just a google group.

--- spec/mailers/notifications_spec.rb
+++ spec/mailers/notifications_spec.rb
@@ -1,15 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe Notifications, type: :mailer do
-  describe '.import_awaiting_approval' do
-    context 'no moderator' do
-      it 'should not raise error' do
-        expect { described_class.import_awaiting_approval(nil) }
-          .to_not raise_error
-      end
-    end
-  end
-
   describe '.send_email' do
     let(:import) do
       double('import').tap do |d|
