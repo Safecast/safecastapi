9d59de7dd4cc03ac7b83e08b5c56d539b50eff37
Eito Katagiri <eitoball@gmail.com>
2017-01-09 11:54:18 +0900

    add spec for BgeigieImports::NotProcessedController

---
+++ spec/controllers/bgeigie_imports/not_processed_controller_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe BgeigieImports::NotProcessedController, type: :controller do
+  before do
+    sign_in Fabricate(:admin_user)
+  end
+
+  describe 'GET #index' do
+    before do
+      get :index
+    end
+
+    it { expect(response).to be_ok }
+    # Cannot use .to be_present because AR::Relation that is empty
+    # returns true to #blank?
+    it { expect(assigns(:bgeigie_imports)).to_not be_nil }
+  end
+end
