2e673674ea9eed7cbea9c14e098d020f2790523a
Mat Schaffer <mat@schaffer.me>
2018-01-11 14:27:47 +0900

    Looks like prod bucket is us-west-2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,13 +18,25 @@ CarrierWave.configure do |config|
     else
       config.storage = :file
     end
-  when 'production', 'staging'
+  when 'staging'
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'us-east-1'
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+    config.fog_public = true
+    config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
+    config.storage = :fog
+  when 'production'
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'us-west-2'
     }
     config.fog_directory = ENV['S3_BUCKET']
     config.fog_public = true
