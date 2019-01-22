d0f377da6be3b7c70df97e591a043f8f5b2b3604
philipp <philipp@nowhere.co.jp>
2019-01-22 11:23:55 +0900

    minimun and maximum added

--- app/controllers/radiation_index_controller.rb
+++ app/controllers/radiation_index_controller.rb
@@ -1,21 +1,28 @@
 class RadiationIndexController < ApplicationController
   def radiation_index
-    require 'csv'
-    @sort_data = read_csv
+    index = 0
+    if params[:index]
+      index = 1 if params[:index].to_sym == :minimum
+      index = 2 if params[:index].to_sym == :maximum
+    end
+    @sort_data = read_csv(index)
   end
 
   private
 
-  def read_csv
-    CSV.foreach('c:\test\g20.csv', headers: true) do |row|
-      @data = row
+  def read_csv(index)
+    require 'csv'
+    @data = []
+    CSV.foreach('/var/deploy/api.safecast.org/web_head/shared/system/g20.csv', headers: true) do |row|
+      @data << row
     end
-    sort_hash(@data)
+    sort_hash(@data[index])
   end
 
   def sort_hash(hash)
     unsort_data = hash.each_with_object({}) do |(k, v), h|
-      h[k] = v.to_f.round(0)
+      v = -1 if v.nil?
+      h[k] = v.to_f.round(1)
     end
     unsort_data.sort_by { |_k, v| -v }
   end
