5c469a5a81231e95ba21e063e9a17df60aa3aa3c
zxc8207 <zxc8207@g.rit.edu>
2019-10-25 15:59:12 +0900

    edit migrate files

--- db/migrate/20191022080113_add_cpm_equals_zero_to_measurement_imports.rb
+++ db/migrate/20191022080113_add_cpm_equals_zero_to_measurement_imports.rb
@@ -2,10 +2,10 @@
 
 class AddCpmEqualsZeroToMeasurementImports < ActiveRecord::Migration
   def up
-    add_column :measurement_imports, :no_zero_cpm, :boolean
+    add_column :measurement_imports, :auto_apprv_no_zero_cpm, :boolean
   end
 
   def down
-    remove_column :measurement_imports, :no_zero_cpm
+    remove_column :measurement_imports, :auto_apprv_no_zero_cpm
   end
 end
