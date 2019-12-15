acd40580bffe7d66bb3fba32c8994d2fafe6cf0d
Eito Katagiri <eitoball@gmail.com>
2019-12-15 22:47:59 +0900

    add database_cleaner

--- lib/plugins/device_story/device_story.gemspec
+++ lib/plugins/device_story/device_story.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency 'rails', '~> 4.2.11'
 
+  s.add_development_dependency 'database_cleaner'
   s.add_development_dependency 'pg', '~> 0.15'
   s.add_development_dependency 'rspec-rails'
   s.add_development_dependency 'rubocop'
--- lib/plugins/device_story/spec/rails_helper.rb
+++ lib/plugins/device_story/spec/rails_helper.rb
@@ -4,12 +4,13 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path('../config/environment', __dir__)
+require File.expand_path('dummy/config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -62,4 +63,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
