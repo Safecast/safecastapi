de0e86eb471fdf136e4c384c3ed5bada06459998
Eito Katagiri <eitoball@gmail.com>
2020-02-01 00:51:49 +0900

    copy attributes from metadata before saving

--- app/models/device_story/device.rb
+++ app/models/device_story/device.rb
@@ -6,5 +6,16 @@ module DeviceStory
 
     validates :device_urn, presence: true, uniqueness: true
     validates :metadata, presence: true
+
+    before_save :update_from_metadata
+
+    private
+
+    def update_from_metadata
+      self.device_id = metadata['device']
+      points = metadata.values_at('last_lon', 'last_lat')
+      self.last_location = "POINT(#{points.join(' ')})" if points.all?(&:present?)
+      self.last_values = metadata['last_values']
+    end
   end
 end
--- spec/models/device_story/device_spec.rb
+++ spec/models/device_story/device_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe DeviceStory::Device, type: :model do
   describe '.create' do
-    it '' do
+    it 'should create from metadata' do
       metadata = <<~__JSON__
         {
           "device": 10041,
@@ -20,7 +20,19 @@ RSpec.describe DeviceStory::Device, type: :model do
           "device_urn": "pointcast:10041"
         }
       __JSON__
-      device = described_class.create(metadata: metadata, comment: '_comment_', user_id: 1)
+      device = described_class.create(
+        device_urn: 'pointcast:10041',
+        metadata: metadata,
+        comment: '_comment_',
+        user_id: 1
+      )
+      expect(device).to have_attributes(
+        device_id: 10_041,
+        last_location: be_present,
+        last_values: '8.3v 58|19cpm'
+      )
+      expect(device.last_location.latitude).to eq(37.796306)
+      expect(device.last_location.longitude).to eq(140.514413)
       expect(device.metadata).to include(
         'device' => 10_041,
         'device_urn' => 'pointcast:10041'
