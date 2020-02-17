c39d5ef462eca94e013d5974e96cb56e574177e6
Mat Schaffer <mat@schaffer.me>
2020-02-17 23:27:28 +0900

    Switch device id limit to 8

--- db/migrate/20200131100913_add_more_columns_to_device_story_devices.rb
+++ db/migrate/20200131100913_add_more_columns_to_device_story_devices.rb
@@ -2,7 +2,7 @@
 
 class AddMoreColumnsToDeviceStoryDevices < ActiveRecord::Migration
   def change
-    add_column :device_story_devices, :device_id, :integer
+    add_column :device_story_devices, :device_id, :integer, limit: 8
     add_column :device_story_devices, :last_location, :st_point, geographic: true
     add_column :device_story_devices, :last_values, :string
   end
