0a4370606a4ae87712fe6458fedbc585f6882944
sasharevzin <sasha.nike@gmail.com>
2020-04-24 22:34:41 -0400

    Fix models spec

--- spec/models/device_story/device_spec.rb
+++ spec/models/device_story/device_spec.rb
@@ -5,24 +5,22 @@ require 'spec_helper'
 RSpec.describe DeviceStory::Device, type: :model do
   describe '.create' do
     it 'should create from metadata' do
-      metadata = <<~__JSON__
-        {
-          "device": 10041,
-          "sn": "10041",
-          "normalized_sn": "10041",
-          "custodian_name": "Larry Richards",
-          "location": "Kamata Azatsukinowayama Fukushima City Fukushima  Japan ",
-          "dashboard": "https://grafana.safecast.cc/d/8MNBALzWk/device-checker-copy?orgId=1&from=now-7d&to=now&refresh=15m&var-device_urn=pointcast:10041",
-          "last_seen": "2019-12-15T13:28:28Z",
-          "last_lat": 37.796306,
-          "last_lon": 140.514413,
-          "last_values": "8.3v 58|19cpm",
-          "device_urn": "pointcast:10041"
-        }
-      __JSON__
+      metadata = {
+        "device" => 10041,
+        "sn" => "10041",
+        "normalized_sn" => "10041",
+        "custodian_name" => "Larry Richards",
+        "location" => "Kamata Azatsukinowayama Fukushima City Fukushima  Japan ",
+        "dashboard" => "https://grafana.safecast.cc/d/8MNBALzWk/device-checker-copy?orgId=1&from=now-7d&to=now&refresh=15m&var-device_urn=pointcast:10041",
+        "last_seen" => "2019-12-15T13:28:28Z",
+        "last_lat" => 37.796306,
+        "last_lon" => 140.514413,
+        "last_values" => "8.3v 58|19cpm",
+        "device_urn" => "pointcast:10041"
+      }
       device = described_class.create(
         device_urn: 'pointcast:10041',
-        metadata: metadata,
+        metadata: metadata.as_json,
         comment: '_comment_',
         user_id: 1
       )
