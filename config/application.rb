4e6e44633f79fe6792cfc951e9e0f7568c2850c1
sasharevzin <sasha.nike@gmail.com>
2020-04-29 17:57:45 -0400

    Set 5.2 defaulta

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Safecast
   class Application < Rails::Application
-    config.load_defaults 5.0
+    config.load_defaults 5.2
     config.active_record.belongs_to_required_by_default = false
 
     # Settings in config/environments/* take precedence over those specified here.
