a7ee79be2f61d5a70e138d1eec815117a163b3a8
sasharevzin <sasha.nike@gmail.com>
2020-05-12 22:45:55 -0400

    Add basic display to device story

---
+++ spec/fabricators/device_story/device_fabricator.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Fabricator(:device_story_device, from: 'DeviceStory::Device') do
+  device_urn { Random.rand(20..100).to_s }
+  metadata do
+    { 'last_seen' => Time.current,
+      'last_values' => Random.rand(20..100).to_s,
+      'last_lon' => '139.719176667',
+      'last_lat' => '35.64214' }
+  end
+  last_location { 'POINT(139.719176667 35.64214)' }
+end
