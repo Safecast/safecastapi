db40d8b641daa6a37b3fdbb4ef12abecf49f4126
Andrew Todd <at@auspicacious.org>
2020-06-04 16:35:07 +0900

    Added db:environment:set invocations.

--- bin/initialize_databases.bash
+++ bin/initialize_databases.bash
@@ -5,14 +5,10 @@ base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd -P )"
 cd "$base_dir"/..
 
 # In some situations the Postgres container may not have started completely yet
-docker-compose run app bash -c "sleep 10 && bundle exec rake db:environment:set RAILS_ENV=development"
-docker-compose run app bundle exec rake db:drop db:create db:structure:load
-docker-compose run app bundle exec rake db:bootstrap
-
+docker-compose run app bash -c "sleep 10 && bundle exec rake db:environment:set RAILS_ENV=development && bundle exec rake db:drop db:create db:structure:load && bundle exec rake db:bootstrap"
 
 # Note that we don't use `db:test:prepare` here but instead use
 # create/load under both the development and test environment. This is
 # due to our use of postgis and structure.sql (as opposed to
 # schema.rb).
-docker-compose run app bash -c "RAILS_ENV='test' bundle exec rake db:environment:set RAILS_ENV=test"
-docker-compose run app bash -c "RAILS_ENV='test' bundle exec rake db:drop db:create db:structure:load"
+docker-compose run app bash -c "RAILS_ENV='test' bundle exec rake db:environment:set RAILS_ENV=test && bundle exec rake db:drop db:create db:structure:load"
