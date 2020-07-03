a13e1de1fa548ae9839573f70fc3a61681c11b6b
Robin <rdeskins17@gmail.com>
2020-07-03 15:21:51 -0700

    Fixed whitespace

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -6,7 +6,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
