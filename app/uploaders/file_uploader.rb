78db9ae125132716223b544a18eb5bf2877c9ede
Robin <rdeskins17@gmail.com>
2020-07-03 15:16:41 -0700

    Fixed indentation

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -6,8 +6,8 @@ class FileUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
-   # Override the directory where uploaded files will be stored.
+  
+  # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
