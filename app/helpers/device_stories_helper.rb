9e8686172b74a0a7d124915e11d9c22a2fd84fcb
Mat Schaffer <mat@elastic.co>
2020-08-28 13:33:25 +0000

    Extract grafana url resolution to helper

---
+++ app/helpers/device_stories_helper.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module DeviceStoriesHelper
+  def grafana_panel(name)
+    panels = {
+      cpm: 14,
+      map: 8
+    }
+    panels[name]
+  end
+
+  def grafana_url(variant = nil)
+    url = ENV['DEVICE_STORIES_GRAFANA_BASE_URL'] || 'https://grafana.safecast.cc/d/DFSxrOLWk/safecast-device-details'
+    uri = URI.parse(url)
+    if variant == :solo
+      parts = uri.path.split('/')
+      parts[1] = 'd-solo'
+      uri.path = parts.join('/')
+    end
+    uri
+  end
+
+  def grafana_more_data(device_urn)
+    url = grafana_url
+    args = {
+      from: 'now-90d',
+      to: 'now',
+      refresh: '15',
+      'var-device_urn': device_urn
+    }
+    url.query = args.to_query
+    url.to_s
+  end
+
+  def grafana_iframe(device_urn, panel_id)
+    url = grafana_url(:solo)
+    args = {
+      from: 'now-90d',
+      to: 'now',
+      refresh: '15',
+      'var-device_urn': device_urn,
+      panelId: panel_id
+    }
+    url.query = args.to_query
+    url.to_s
+  end
+end
---
+++ spec/helpers/device_stories_helper_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+RSpec.describe DeviceStoriesHelper, type: :helper do
+  device_urn = 'local:12345'
+  base_url = 'http://grafana.127.0.0.1.xip.io/d/DFSxrOLWk/safecast-device-details'
+
+  before do
+    ENV['DEVICE_STORIES_GRAFANA_BASE_URL'] = base_url
+  end
+
+  it 'provides a more details grafana link' do
+    url = helper.grafana_more_data(device_urn)
+    expect(url).to match '12345'
+    expect(url).to match base_url
+  end
+
+  it 'provides solo panel iframe urls' do
+    url = helper.grafana_iframe(device_urn, helper.grafana_panel(:cpm))
+    expect(url).to match 'd-solo'
+    expect(url).to match '12345'
+    expect(url).to match '14'
+  end
+end
