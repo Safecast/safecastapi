e593f39b25ecfaea1107b03477efe75cb22fb07a
Nakushi64 <40150445+Nakushi64@users.noreply.github.com>
2020-12-04 12:33:09 +0900

    Fixed rspec issue

--- spec/models/device_story_comment_spec.rb
+++ spec/models/device_story_comment_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe DeviceStoryComment, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
