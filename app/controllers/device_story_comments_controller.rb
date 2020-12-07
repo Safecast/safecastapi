721f423d50d308ed9f7ddc86a74dd346dc24bb3f
Nakushi64 <40150445+Nakushi64@users.noreply.github.com>
2020-12-08 08:43:47 +0900

    Rubocop changes

--- app/controllers/device_story_comments_controller.rb
+++ app/controllers/device_story_comments_controller.rb
@@ -1,28 +1,22 @@
 # frozen_string_literal: true
 
 class DeviceStoryCommentsController < ApplicationController
-  before_action :get_device_story
-  before_action :set_device_story_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_device_story
+  before_action :set_device_story_comment, only: %i(show edit update destroy)
 
   def index
     @device_story_comments = @device_story.device_story_comments
-  end
-
-  def show
   end
 
   def new
     @device_story_comment = @device_story.device_story_comments.build
   end
 
-  def edit
-  end
-
   def create
     @device_story_comment = @device_story.device_story_comments.build(device_story_comment_params)
     respond_to do |format|
       if @device_story_comment.save
-        format.js {render inline: "location.reload();" }
+        format.js { render inline: 'location.reload();' }
       else
         format.html { render :new }
         format.json { render json: @device_story_comment.errors, status: :unprocessable_entity }
@@ -58,7 +52,7 @@ class DeviceStoryCommentsController < ApplicationController
     params.require(:device_story_comment).permit(:content, :device_story_id, :user_id)
   end
 
-  def get_device_story
+  def set_device_story
     @device_story = DeviceStory.find(params[:device_story_id])
   end
 end
