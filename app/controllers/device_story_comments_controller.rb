4d3a04ce9ce2dfb838cc93a468fac97288dd1e55
Nakushi64 <40150445+Nakushi64@users.noreply.github.com>
2021-01-05 18:41:38 +0900

    Applied suggested changes

--- app/controllers/device_story_comments_controller.rb
+++ app/controllers/device_story_comments_controller.rb
@@ -13,33 +13,45 @@ class DeviceStoryCommentsController < ApplicationController
   end
 
   def create
+    if user_signed_in?
     @device_story_comment = @device_story.device_story_comments.build(device_story_comment_params)
-    respond_to do |format|
-      if @device_story_comment.save && !@device_story_comment.spam?
-        format.html { redirect_to device_story_path(@device_story), notice: 'Comment successfully submitted!' }
-      else
-        flash[:error] = 'Could not submit your comment. Please, try again!'
-        format.html { redirect_to device_story_path(@device_story) }
+    @device_story_comment.user_id = current_user.id
+      respond_to do |format|
+        if @device_story_comment.save && !@device_story_comment.spam?
+          format.html { redirect_to device_story_path(@device_story), notice: 'Comment successfully submitted!' }
+        else
+          flash[:error] = 'Could not submit your comment: ' + @device_story_comment.errors.full_messages.join(" ") + '!'
+          format.html { redirect_to device_story_path(@device_story) }
+        end
       end
     end
   end
 
+
   def update
-    respond_to do |format|
-      if @device_story_comment.update(device_story_comment_params)
-        format.html { redirect_to device_story_path(@device_story), notice: 'device_story_comment was successfully updated.' }
-        format.json { render :show, status: :ok, location: @device_story_comment }
-      else
-        format.html { render :edit }
-        format.json { render json: @device_story_comment.errors, status: :unprocessable_entity }
+    if user_signed_in?
+      if (@device_story_comment.user_id == current_user.id) || moderator?(current_user)
+        respond_to do |format|
+          if @device_story_comment.update(device_story_comment_params)
+            format.html { redirect_to device_story_path(@device_story), notice: 'device_story_comment was successfully updated.' }
+            format.json { render :show, status: :ok, location: @device_story_comment }
+          else
+            format.html { render :edit }
+            format.json { render json: @device_story_comment.errors, status: :unprocessable_entity }
+          end
+        end
       end
     end
   end
 
   def destroy
-    @device_story_comment.destroy
-    respond_to do |format|
-      format.html { redirect_to device_story_path(@device_story), notice: 'Comment deleted!' }
+    if user_signed_in?
+      if (@device_story_comment.user_id == current_user.id) || moderator?(current_user)
+        @device_story_comment.destroy
+        respond_to do |format|
+          format.html { redirect_to device_story_path(@device_story), notice: 'Comment deleted!' }
+        end
+      end
     end
   end
 
@@ -50,7 +62,6 @@ class DeviceStoryCommentsController < ApplicationController
   end
 
   def device_story_comment_params
-    params.inspect
     params.require(:device_story_comment).permit(:content, :device_story_id, :user_id)
   end
 
