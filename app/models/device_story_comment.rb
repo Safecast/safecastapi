62d7cfd11df00621d4e53c8bed54f6364678d044
Nakushi64 <40150445+Nakushi64@users.noreply.github.com>
2021-01-06 10:05:40 +0900

    Update app/models/device_story_comment.rb
    Co-authored-by: Eito Katagiri <eitoball@gmail.com>

--- app/models/device_story_comment.rb
+++ app/models/device_story_comment.rb
@@ -4,7 +4,6 @@ class DeviceStoryComment < ApplicationRecord
   include Rakismet::Model
   validates :content, presence: true, length: { maximum: 1000 }
   validates :user_id, presence: true
-  validates :device_story_id, presence: true
-  belongs_to :device_story
-  belongs_to :user
+  belongs_to :device_story, require: true
+  belongs_to :user, require: true
 end
