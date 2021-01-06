d9c5ba811dfb406943098f4092537beb5f70d5d6
Nakushi64 <40150445+Nakushi64@users.noreply.github.com>
2021-01-06 10:07:03 +0900

    Update config/initializers/rakismet.rb
    Co-authored-by: Mat Schaffer <mat@schaffer.me>

--- config/initializers/rakismet.rb
+++ config/initializers/rakismet.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 Safecast::Application.config.rakismet.key = ENV['AKISMET_API_KEY']
-Safecast::Application.config.rakismet.url = 'http://safecast.com/'
+Safecast::Application.config.rakismet.url = "https://#{ENV['DEFAULT_HOSTNAME']}/"
