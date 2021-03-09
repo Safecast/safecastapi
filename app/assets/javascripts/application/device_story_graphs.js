ceb9ca38e105417b6a1ed1a2f3323e33a386ac96
Nakushi64 <ichigo_link@yahoo.de>
2021-03-09 19:33:16 +0100

    Fix some Codeclimate issues

--- app/assets/javascripts/application/device_story_graphs.js
+++ app/assets/javascripts/application/device_story_graphs.js
@@ -3,26 +3,28 @@ $(document).on('change','#temp-switch',function(){
     {
         $('#celsius').addClass('hidden');
         $('#fahrenheit').removeClass('hidden');
-        window.dispatchEvent(new Event('resize'))
     }
     else
     {
         $('#fahrenheit').addClass('hidden');
         $('#celsius').removeClass('hidden');
-        window.dispatchEvent(new Event('resize'))
     }
+    window.dispatchEvent(new Event('resize'))
 });
+
 $(document).on('change',"[id^='width-switch']",function(){
     if(this.checked)
     {
-        $(this).parent().parent().parent().parent().removeClass();
-        $(this).parent().parent().parent().parent().addClass('col-md-12');
-        window.dispatchEvent(new Event('resize'))
+        setClass($(this),'col-md-12');
     }
     else
     {
-        $(this).parent().parent().parent().parent().removeClass();
-        $(this).parent().parent().parent().parent().addClass('col-md-6');
-        window.dispatchEvent(new Event('resize'))
+        setClass($(this),'col-md-6');
     }
 });
+function setClass(obj, c)
+{
+    obj.parent().parent().parent().parent().removeClass();
+    obj.parent().parent().parent().parent().addClass(c);
+    window.dispatchEvent(new Event('resize'))
+}
