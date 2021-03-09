40b15cb7f2345c01fc8bc0d3a91225ef5ab32223
Eito Katagiri <eitoball@gmail.com>
2021-03-09 09:25:57 +0900

    Remove presence validation of custodian_name (device_contact_name) from DeviceStory (#784)
    * bump webmock from 3.8.3 to 3.12.1
    
    * remove presence validation of device_contact_name (custodian_name) attribute in DeviceStory model

--- app/models/device_story.rb
+++ app/models/device_story.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class DeviceStory < ApplicationRecord
-  validates :device_urn, uniqueness: true
-  validates :device_urn, :custodian_name, presence: true
+  validates :device_urn, uniqueness: true, presence: true
   has_many  :device_story_comments, dependent: :destroy
 
   def update_from_ttserve(metadata)
