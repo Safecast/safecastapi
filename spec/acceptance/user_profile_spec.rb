60fbe572281acfc2ade41d4c02f2f5c0d72051e0
Eito Katagiri <eitoball@gmail.com>
2021-12-26 20:51:15 +0900

    Reset to default locale (#854)
    * reset to default locale after finishing running specs
    
    * fix ActiveStorage::IntegrityError that occurs under some conditions
    
    https://github.com/rails/rails/issues/41991
    
    * Update spec/models/device_story_comment_spec.rb
    
    Co-authored-by: Mat Schaffer <mat@schaffer.me>
    
    Co-authored-by: Mat Schaffer <mat@schaffer.me>

--- spec/acceptance/user_profile_spec.rb
+++ spec/acceptance/user_profile_spec.rb
@@ -6,6 +6,8 @@ feature 'User Profile', type: :feature do
   let(:user) { Fabricate(:user) }
   before { sign_in(user) }
 
+  after(:all) { I18n.locale = I18n.default_locale }
+
   scenario 'viewing a user profile' do
     expect(page).to have_content(user.authentication_token)
   end
