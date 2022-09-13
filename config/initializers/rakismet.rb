eeb3a3c6a84fcce2e188cd5313f7e3082a4cf862
Eito Katagiri <eitoball@gmail.com>
2022-09-13 11:28:29 +0900

    correct Style/FetchEnvVar

--- config/initializers/rakismet.rb
+++ config/initializers/rakismet.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-Safecast::Application.config.rakismet.key = ENV['AKISMET_API_KEY']
-Safecast::Application.config.rakismet.url = "https://#{ENV['DEFAULT_HOSTNAME']}/"
+Safecast::Application.config.rakismet.key = ENV.fetch('AKISMET_API_KEY', nil)
+Safecast::Application.config.rakismet.url = "https://#{ENV.fetch('DEFAULT_HOSTNAME', nil)}/"
