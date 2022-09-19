06a0f56183097f402b6603a357bb56a8c9bd49c5
Eito Katagiri <eitoball@gmail.com>
2022-09-19 14:33:29 +0900

    migrate Coffeescript to Javascript

---
+++ app/assets/javascripts/application/open_all.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function () {
+  $("#open_all").click(function () {
+    import_url_list = $(this).data('bgeigie');
+    if (import_url_list.length != 0) {
+      for (bgeigie_import_url in import_url_list) {
+        window.open(bgeigie_import_url)
+      }
+    } else {
+      alert("There are no unmoderated files on this side!");
+    }
+  });
+});
