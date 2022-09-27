c1ab3a32d0f9c9534707fa54978e0671bdb8a3d1
Eito Katagiri <eitoball@gmail.com>
2022-09-27 17:01:01 +0900

    add "GET /users/me" that returns current logged-in user information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,13 @@
 class UsersController < ApplicationController
   include Swagger::Blocks
 
+  before_action :authenticate_user!, only: %i(me)
+
   has_scope :order do |_controller, scope, value|
     # TODO: Use `nulls_last` Arel method in Rails 6.1
     scope.order("#{value} NULLS LAST")
   end
+
   has_scope :name do |_controller, scope, value|
     scope.by_name(value)
   end
@@ -35,5 +38,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     respond_with @user
+  end
+
+  def me
+    @user = current_user
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @user }
+    end
   end
 end
